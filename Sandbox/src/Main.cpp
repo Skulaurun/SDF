9d56acc17e6ba93319acf7fc356f124f7d6e6a61
Skulaurun Mrusal <Skulaurun@seznam.cz>
2021-12-30 20:37:59 +0100

    Update example with new event system

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -1,9 +1,34 @@
 #include <SDF/SDF.hpp>
 
+#include <iostream>
+
+void onWindowResize(const sdf::WindowResizeEvent& e) {
+    std::cout << "Window resized: (" << e.getWidth() << ", " << e.getHeight() << ")\n";
+}
+
+void onWindowMove(const sdf::WindowMoveEvent& e) {
+    std::cout << "Window moved: (" << e.getX() << ", " << e.getY() << ")\n";
+}
+
+void onWindowEvent(const sdf::WindowEvent& e) {
+
+    // Dispatch events we want to handle,
+    // the dispatch method takes care of the event type checking
+    e.dispatch<sdf::WindowMoveEvent>(onWindowMove);
+    e.dispatch<sdf::WindowResizeEvent>(onWindowResize);
+
+    // Invokes the default behavior for certain events
+    e.defaultDispatch();
+
+}
+
 int main() {
 
     // Create the window
     sdf::Window window(L"Sandbox App", 800, 600);
+
+    // Use custom callback for event handling
+    window.setEventCallback(onWindowEvent);
 
     // Make the window visible
     window.show();
