546a9e582a560ba026439e1bcfc752032de29e0a
Skulaurun Mrusal <Skulaurun@seznam.cz>
2022-01-14 23:44:59 +0100

    Add WindowKeyboardEvent

--- SDF/include/SDF/Core/WindowEvent.hpp
+++ SDF/include/SDF/Core/WindowEvent.hpp
@@ -2,6 +2,7 @@
 
 #include "Event.hpp"
 #include "Window.hpp"
+#include "Input.hpp"
 
 namespace sdf {
 
@@ -59,6 +60,56 @@ namespace sdf {
 
     private:
         uint32_t width, height;
+
+    };
+
+    class WindowInputEvent : public WindowEvent {
+
+    public:
+        WindowInputEvent(
+            Window& window,
+            uint8_t mask
+        ) :
+            WindowEvent(window),
+            ctrlPressed((mask >> 0) & 1u),
+            shiftPressed((mask >> 1) & 1u),
+            altPressed((mask >> 2) & 1u)
+        {}
+
+        bool isCtrlPressed() const { return ctrlPressed; }
+        bool isShiftPressed() const { return shiftPressed; }
+        bool isAltPressed() const { return altPressed; }
+
+        virtual const std::type_info& getType() const override { return typeid(WindowInputEvent); }
+
+    private:
+        bool ctrlPressed;
+        bool shiftPressed;
+        bool altPressed;
+
+    };
+
+    class WindowKeyboardEvent : public WindowInputEvent {
+
+    public:
+        WindowKeyboardEvent(
+            Window& window,
+            uint8_t mask,
+            Input::Key key
+        ) :
+            WindowInputEvent(window, mask),
+            keyPressed((mask >> 3) & 1u),
+            key(key)
+        {}
+
+        Input::Key getKey() const { return key; }
+        bool isKeyPressed() const { return keyPressed; }
+
+        virtual const std::type_info& getType() const override { return typeid(WindowKeyboardEvent); }
+
+    private:
+        Input::Key key;
+        bool keyPressed;
 
     };
 
