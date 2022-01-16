6d3eb11c40083b57cad4a957ff946a397dbef645
Skulaurun Mrusal <Skulaurun@seznam.cz>
2022-01-16 18:36:00 +0100

    Update SDF.hpp includes

--- SDF/include/SDF/SDF.hpp
+++ SDF/include/SDF/SDF.hpp
@@ -1,7 +1,16 @@
 #pragma once
 
+/* Core Module */
+
 #include "Core/Exception.hpp"
+
+#include "Core/Vector.hpp"
 
 #include "Core/Time.hpp"
 #include "Core/Input.hpp"
 #include "Core/Window.hpp"
+
+/* Graphics Module */
+
+#include "Graphics/Matrix.hpp"
+#include "Graphics/Math.hpp"
