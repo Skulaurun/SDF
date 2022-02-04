b19f266b1147d43d2a2b8599581e40c959a70f43
Skulaurun Mrusal <Skulaurun@seznam.cz>
2022-02-04 18:41:44 +0100

    Update: Improve OpenGL Loader (with macro)

--- SDF/src/Graphics/OpenGL/OpenGL.cpp
+++ SDF/src/Graphics/OpenGL/OpenGL.cpp
@@ -4,106 +4,108 @@
 
 #pragma comment (lib, "opengl32.lib")
 
+#define LOAD_GL_FUNCTION(type, function) function = (type)getGLFunction(#function)
+
 static HMODULE glLibrary;
 
 static void* getGLFunction(const char* name) {
+    
+    void* ptr = (void*)wglGetProcAddress(name);
+    if (ptr == 0) {
+        ptr = (void*)GetProcAddress(glLibrary, name);
+    }
 
-	void* ptr = (void*)wglGetProcAddress(name);
-	if (ptr == 0) {
-		ptr = (void*)GetProcAddress(glLibrary, name);
-	}
-
-	return ptr;
+    return ptr;
 
 }
 
 namespace sdf {
 
-	GLClearPtr glClear = nullptr;
-	GLClearColorPtr glClearColor = nullptr;
-	GLViewportPtr glViewport = nullptr;
-	GLGetStringPtr glGetString = nullptr;
-	GLGenBuffersPtr glGenBuffers = nullptr;
-	GLDeleteBuffersPtr glDeleteBuffers = nullptr;
-	GLBindBufferPtr glBindBuffer = nullptr;
-	GLBufferDataPtr glBufferData = nullptr;
-	GLBufferSubDataPtr glBufferSubData = nullptr;
-	GLGenVertexArraysPtr glGenVertexArrays = nullptr;
-	GLDeleteVertexArraysPtr glDeleteVertexArrays = nullptr;
-	GLBindVertexArrayPtr glBindVertexArray = nullptr;
-	GLDrawElementsPtr glDrawElements = nullptr;
-	GLDrawArraysPtr glDrawArrays = nullptr;
-	GLVertexAttribPointerPtr glVertexAttribPointer = nullptr;
-	GLEnableVertexAttribArrayPtr glEnableVertexAttribArray = nullptr;
-	GLCreateProgramPtr glCreateProgram = nullptr;
-	GLDeleteProgramPtr glDeleteProgram = nullptr;
-	GLLinkProgramPtr glLinkProgram = nullptr;
-	GLUseProgramPtr glUseProgram = nullptr;
-	GLCreateShaderPtr glCreateShader = nullptr;
-	GLShaderSourcePtr glShaderSource = nullptr;
-	GLCompileShaderPtr glCompileShader = nullptr;
-	GLGetShaderivPtr glGetShaderiv = nullptr;
-	GLGetShaderInfoLogPtr glGetShaderInfoLog = nullptr;
-	GLAttachShaderPtr glAttachShader = nullptr;
-	GLDeleteShaderPtr glDeleteShader = nullptr;
-	GLGetUniformLocationPtr glGetUniformLocation = nullptr;
-	GLUniform1fPtr glUniform1f = nullptr;
-	GLUniform1iPtr glUniform1i = nullptr;
-	GLUniform2fvPtr glUniform2fv = nullptr;
-	GLUniform3fvPtr glUniform3fv = nullptr;
-	GLUniform4fvPtr glUniform4fv = nullptr;
-	GLUniformMatrix2fvPtr glUniformMatrix2fv = nullptr;
-	GLUniformMatrix3fvPtr glUniformMatrix3fv = nullptr;
-	GLUniformMatrix4fvPtr glUniformMatrix4fv = nullptr;
+    GLClearPtr                      glClear                     = nullptr;
+    GLClearColorPtr                 glClearColor                = nullptr;
+    GLViewportPtr                   glViewport                  = nullptr;
+    GLGetStringPtr                  glGetString                 = nullptr;
+    GLGenBuffersPtr                 glGenBuffers                = nullptr;
+    GLDeleteBuffersPtr              glDeleteBuffers             = nullptr;
+    GLBindBufferPtr                 glBindBuffer                = nullptr;
+    GLBufferDataPtr                 glBufferData                = nullptr;
+    GLBufferSubDataPtr              glBufferSubData             = nullptr;
+    GLGenVertexArraysPtr            glGenVertexArrays           = nullptr;
+    GLDeleteVertexArraysPtr         glDeleteVertexArrays        = nullptr;
+    GLBindVertexArrayPtr            glBindVertexArray           = nullptr;
+    GLDrawElementsPtr               glDrawElements              = nullptr;
+    GLDrawArraysPtr                 glDrawArrays                = nullptr;
+    GLVertexAttribPointerPtr        glVertexAttribPointer       = nullptr;
+    GLEnableVertexAttribArrayPtr    glEnableVertexAttribArray   = nullptr;
+    GLCreateProgramPtr              glCreateProgram             = nullptr;
+    GLDeleteProgramPtr              glDeleteProgram             = nullptr;
+    GLLinkProgramPtr                glLinkProgram               = nullptr;
+    GLUseProgramPtr                 glUseProgram                = nullptr;
+    GLCreateShaderPtr               glCreateShader              = nullptr;
+    GLShaderSourcePtr               glShaderSource              = nullptr;
+    GLCompileShaderPtr              glCompileShader             = nullptr;
+    GLGetShaderivPtr                glGetShaderiv               = nullptr;
+    GLGetShaderInfoLogPtr           glGetShaderInfoLog          = nullptr;
+    GLAttachShaderPtr               glAttachShader              = nullptr;
+    GLDeleteShaderPtr               glDeleteShader              = nullptr;
+    GLGetUniformLocationPtr         glGetUniformLocation        = nullptr;
+    GLUniform1fPtr                  glUniform1f                 = nullptr;
+    GLUniform1iPtr                  glUniform1i                 = nullptr;
+    GLUniform2fvPtr                 glUniform2fv                = nullptr;
+    GLUniform3fvPtr                 glUniform3fv                = nullptr;
+    GLUniform4fvPtr                 glUniform4fv                = nullptr;
+    GLUniformMatrix2fvPtr           glUniformMatrix2fv          = nullptr;
+    GLUniformMatrix3fvPtr           glUniformMatrix3fv          = nullptr;
+    GLUniformMatrix4fvPtr           glUniformMatrix4fv          = nullptr;
 
-	bool initGL() {
+    bool loadGLFunctions() {
 
-		glLibrary = LoadLibrary("opengl32.dll");
-		if (glLibrary == NULL) {
-			return false;
-		}
+        glLibrary = LoadLibrary("opengl32.dll");
+        if (glLibrary == NULL) {
+            return false;
+        }
 
-		glClear = (GLClearPtr)getGLFunction("glClear");
-		glClearColor = (GLClearColorPtr)getGLFunction("glClearColor");
-		glViewport = (GLViewportPtr)getGLFunction("glViewport");
-		glGetString = (GLGetStringPtr)getGLFunction("glGetString");
-		glGenBuffers = (GLGenBuffersPtr)getGLFunction("glGenBuffers");
-		glDeleteBuffers = (GLDeleteBuffersPtr)getGLFunction("glDeleteBuffers");
-		glBindBuffer = (GLBindBufferPtr)getGLFunction("glBindBuffer");
-		glBufferData = (GLBufferDataPtr)getGLFunction("glBufferData");
-		glBufferSubData = (GLBufferSubDataPtr)getGLFunction("glBufferSubData");
-		glGenVertexArrays = (GLGenVertexArraysPtr)getGLFunction("glGenVertexArrays");
-		glDeleteVertexArrays = (GLDeleteVertexArraysPtr)getGLFunction("glDeleteVertexArrays");
-		glBindVertexArray = (GLBindVertexArrayPtr)getGLFunction("glBindVertexArray");
-		glDrawElements = (GLDrawElementsPtr)getGLFunction("glDrawElements");
-		glDrawArrays = (GLDrawArraysPtr)getGLFunction("glDrawArrays");
-		glVertexAttribPointer = (GLVertexAttribPointerPtr)getGLFunction("glVertexAttribPointer");
-		glEnableVertexAttribArray = (GLEnableVertexAttribArrayPtr)getGLFunction("glEnableVertexAttribArray");
-		glCreateProgram = (GLCreateProgramPtr)getGLFunction("glCreateProgram");
-		glDeleteProgram = (GLDeleteProgramPtr)getGLFunction("glDeleteProgram");
-		glLinkProgram = (GLLinkProgramPtr)getGLFunction("glLinkProgram");
-		glUseProgram = (GLUseProgramPtr)getGLFunction("glUseProgram");
-		glCreateShader = (GLCreateShaderPtr)getGLFunction("glCreateShader");
-		glShaderSource = (GLShaderSourcePtr)getGLFunction("glShaderSource");
-		glCompileShader = (GLCompileShaderPtr)getGLFunction("glCompileShader");
-		glGetShaderiv = (GLGetShaderivPtr)getGLFunction("glGetShaderiv");
-		glGetShaderInfoLog = (GLGetShaderInfoLogPtr)getGLFunction("glGetShaderInfoLog");
-		glAttachShader = (GLAttachShaderPtr)getGLFunction("glAttachShader");
-		glDeleteShader = (GLDeleteShaderPtr)getGLFunction("glDeleteShader");
-		glGetUniformLocation = (GLGetUniformLocationPtr)getGLFunction("glGetUniformLocation");
-		glUniform1f = (GLUniform1fPtr)getGLFunction("glUniform1f");
-		glUniform1i = (GLUniform1iPtr)getGLFunction("glUniform1i");
-		glUniform2fv = (GLUniform2fvPtr)getGLFunction("glUniform2fv");
-		glUniform3fv = (GLUniform3fvPtr)getGLFunction("glUniform3fv");
-		glUniform4fv = (GLUniform4fvPtr)getGLFunction("glUniform4fv");
-		glUniformMatrix2fv = (GLUniformMatrix2fvPtr)getGLFunction("glUniformMatrix2fv");
-		glUniformMatrix3fv = (GLUniformMatrix3fvPtr)getGLFunction("glUniformMatrix3fv");
-		glUniformMatrix4fv = (GLUniformMatrix4fvPtr)getGLFunction("glUniformMatrix4fv");
-	
-		// FreeLibrary
+        LOAD_GL_FUNCTION(GLClearPtr, glClear);
+        LOAD_GL_FUNCTION(GLClearColorPtr, glClearColor);
+        LOAD_GL_FUNCTION(GLViewportPtr, glViewport);
+        LOAD_GL_FUNCTION(GLGetStringPtr, glGetString);
+        LOAD_GL_FUNCTION(GLGenBuffersPtr, glGenBuffers);
+        LOAD_GL_FUNCTION(GLDeleteBuffersPtr, glDeleteBuffers);
+        LOAD_GL_FUNCTION(GLBindBufferPtr, glBindBuffer);
+        LOAD_GL_FUNCTION(GLBufferDataPtr, glBufferData);
+        LOAD_GL_FUNCTION(GLBufferSubDataPtr, glBufferSubData);
+        LOAD_GL_FUNCTION(GLGenVertexArraysPtr, glGenVertexArrays);
+        LOAD_GL_FUNCTION(GLDeleteVertexArraysPtr, glDeleteVertexArrays);
+        LOAD_GL_FUNCTION(GLBindVertexArrayPtr, glBindVertexArray);
+        LOAD_GL_FUNCTION(GLDrawElementsPtr, glDrawElements);
+        LOAD_GL_FUNCTION(GLDrawArraysPtr, glDrawArrays);
+        LOAD_GL_FUNCTION(GLVertexAttribPointerPtr, glVertexAttribPointer);
+        LOAD_GL_FUNCTION(GLEnableVertexAttribArrayPtr, glEnableVertexAttribArray);
+        LOAD_GL_FUNCTION(GLCreateProgramPtr, glCreateProgram);
+        LOAD_GL_FUNCTION(GLDeleteProgramPtr, glDeleteProgram);
+        LOAD_GL_FUNCTION(GLLinkProgramPtr, glLinkProgram);
+        LOAD_GL_FUNCTION(GLUseProgramPtr, glUseProgram);
+        LOAD_GL_FUNCTION(GLCreateShaderPtr, glCreateShader);
+        LOAD_GL_FUNCTION(GLShaderSourcePtr, glShaderSource);
+        LOAD_GL_FUNCTION(GLCompileShaderPtr, glCompileShader);
+        LOAD_GL_FUNCTION(GLGetShaderivPtr, glGetShaderiv);
+        LOAD_GL_FUNCTION(GLGetShaderInfoLogPtr, glGetShaderInfoLog);
+        LOAD_GL_FUNCTION(GLAttachShaderPtr, glAttachShader);
+        LOAD_GL_FUNCTION(GLDeleteShaderPtr, glDeleteShader);
+        LOAD_GL_FUNCTION(GLGetUniformLocationPtr, glGetUniformLocation);
+        LOAD_GL_FUNCTION(GLUniform1fPtr, glUniform1f);
+        LOAD_GL_FUNCTION(GLUniform1iPtr, glUniform1i);
+        LOAD_GL_FUNCTION(GLUniform2fvPtr, glUniform2fv);
+        LOAD_GL_FUNCTION(GLUniform3fvPtr, glUniform3fv);
+        LOAD_GL_FUNCTION(GLUniform4fvPtr, glUniform4fv);
+        LOAD_GL_FUNCTION(GLUniformMatrix2fvPtr, glUniformMatrix2fv);
+        LOAD_GL_FUNCTION(GLUniformMatrix3fvPtr, glUniformMatrix3fv);
+        LOAD_GL_FUNCTION(GLUniformMatrix4fvPtr, glUniformMatrix4fv);
 
-		return true;
+        // FreeLibrary
 
-	}
+        return true;
+
+    }
 
 }
--- SDF/src/Graphics/OpenGL/OpenGL.hpp
+++ SDF/src/Graphics/OpenGL/OpenGL.hpp
@@ -4,118 +4,118 @@
 
 namespace sdf {
 
-	enum {
-		GL_TRUE = 1,
-		GL_FALSE = 0,
-		GL_VERSION = 0x1F02,
-		GL_TRIANGLES = 0x0004,
-		GL_ARRAY_BUFFER = 0x8892,
-		GL_ELEMENT_ARRAY_BUFFER = 0x8893,
-		GL_STATIC_DRAW = 0x88E4,
-		GL_DYNAMIC_DRAW = 0x88E8,
-		GL_COLOR_BUFFER_BIT = 0x00004000,
-		GL_FLOAT = 0x1406,
-		GL_UNSIGNED_INT = 0x1405,
-		GL_VERTEX_SHADER = 0x8B31,
-		GL_FRAGMENT_SHADER = 0x8B30,
-		GL_COMPILE_STATUS = 0x8B81,
-		GL_INFO_LOG_LENGTH = 0x8B84
-	};
+    enum {
+        GL_TRUE = 1,
+        GL_FALSE = 0,
+        GL_VERSION = 0x1F02,
+        GL_TRIANGLES = 0x0004,
+        GL_ARRAY_BUFFER = 0x8892,
+        GL_ELEMENT_ARRAY_BUFFER = 0x8893,
+        GL_STATIC_DRAW = 0x88E4,
+        GL_DYNAMIC_DRAW = 0x88E8,
+        GL_COLOR_BUFFER_BIT = 0x00004000,
+        GL_FLOAT = 0x1406,
+        GL_UNSIGNED_INT = 0x1405,
+        GL_VERTEX_SHADER = 0x8B31,
+        GL_FRAGMENT_SHADER = 0x8B30,
+        GL_COMPILE_STATUS = 0x8B81,
+        GL_INFO_LOG_LENGTH = 0x8B84
+    };
 
-	#if defined(_WIN64)
-		using GLintptr = signed long long int;
-		using GLsizeiptr = signed long long int;
-	#else
-		using GLintptr = signed long int;
-		using GLsizeiptr = signed long int;
-	#endif
+    #if defined(_WIN64)
+        using GLintptr = signed long long int;
+        using GLsizeiptr = signed long long int;
+    #else
+        using GLintptr = signed long int;
+        using GLsizeiptr = signed long int;
+    #endif
 
-	using GLchar = char;
-	using GLbyte = signed char;
-	using GLubyte = unsigned char;
-	using GLboolean = unsigned char;
-	using GLint = signed int;
-	using GLuint = unsigned int;
-	using GLenum = unsigned int;
-	using GLsizei = unsigned int; // unsigned?
-	using GLbitfield = unsigned int;
-	using GLfloat = float;
+    using GLchar = char;
+    using GLbyte = signed char;
+    using GLubyte = unsigned char;
+    using GLboolean = unsigned char;
+    using GLint = signed int;
+    using GLuint = unsigned int;
+    using GLenum = unsigned int;
+    using GLsizei = unsigned int; // unsigned?
+    using GLbitfield = unsigned int;
+    using GLfloat = float;
 
-	// Prepend with '_'?
-	using GLClearPtr = void (__stdcall*)(GLbitfield mask);
-	using GLClearColorPtr = void (__stdcall*)(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha);
-	using GLViewportPtr = void (__stdcall*)(GLint x, GLint y, GLsizei width, GLsizei height);
-	using GLGetStringPtr = const GLubyte* (__stdcall*)(GLenum name);
-	using GLGenBuffersPtr = void (__stdcall*)(GLsizei n, GLuint* buffers);
-	using GLDeleteBuffersPtr = void (__stdcall*)(GLsizei n, const GLuint* buffers);
-	using GLBindBufferPtr = void (__stdcall*)(GLenum target, GLuint buffer);
-	using GLBufferDataPtr = void (__stdcall*)(GLenum target, GLsizeiptr size, const void* data, GLenum usage);
-	using GLBufferSubDataPtr = void (__stdcall*)(GLenum target, GLintptr offset, GLsizeiptr size, const void* data);
-	using GLGenVertexArraysPtr = void(__stdcall*)(GLsizei n, GLuint* arrays);
-	using GLDeleteVertexArraysPtr = void(__stdcall*)(GLsizei n, const GLuint* arrays);
-	using GLBindVertexArrayPtr = void(__stdcall*)(GLuint array);
-	using GLDrawElementsPtr = void (__stdcall*)(GLenum mode, GLsizei count, GLenum type, const void* indices);
-	using GLDrawArraysPtr = void (__stdcall*)(GLenum mode, GLint first, GLsizei count);
-	using GLVertexAttribPointerPtr = void (__stdcall*)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void* pointer);
-	using GLEnableVertexAttribArrayPtr = void (__stdcall*)(GLuint index);
-	using GLCreateProgramPtr = GLuint (__stdcall*)(void);
-	using GLDeleteProgramPtr = void (__stdcall*)(GLuint program);
-	using GLLinkProgramPtr = void (__stdcall*)(GLuint program);
-	using GLUseProgramPtr = void (__stdcall*)(GLuint program);
-	using GLCreateShaderPtr = GLuint (__stdcall*)(GLenum shaderType);
-	using GLShaderSourcePtr = void (__stdcall*)(GLuint shader, GLsizei count, const GLchar** string, const GLint* length);
-	using GLCompileShaderPtr = void (__stdcall*)(GLuint shader);
-	using GLGetShaderivPtr = void (__stdcall*)(GLuint shader, GLenum pname, GLint* params);
-	using GLGetShaderInfoLogPtr = void (__stdcall*)(GLuint shader, GLsizei maxLength, GLsizei* length, GLchar* infoLog);
-	using GLAttachShaderPtr = void (__stdcall*)(GLuint program, GLuint shader);
-	using GLDeleteShaderPtr = void (__stdcall*)(GLuint shader);
-	using GLGetUniformLocationPtr = GLint (__stdcall*)(GLuint program, const GLchar* name);
-	using GLUniform1fPtr = void (__stdcall*)(GLint location, GLfloat v0);
-	using GLUniform1iPtr = void (__stdcall*)(GLint location, GLint v0);
-	using GLUniform2fvPtr = void (__stdcall*)(GLint location, GLsizei count, const GLfloat* value);
-	using GLUniform3fvPtr = void (__stdcall*)(GLint location, GLsizei count, const GLfloat* value);
-	using GLUniform4fvPtr = void (__stdcall*)(GLint location, GLsizei count, const GLfloat* value);
-	using GLUniformMatrix2fvPtr = void (__stdcall*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value);
-	using GLUniformMatrix3fvPtr = void (__stdcall*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value);
-	using GLUniformMatrix4fvPtr = void (__stdcall*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value);
+    // Prepend with '_'?
+    using GLClearPtr = void (__stdcall*)(GLbitfield mask);
+    using GLClearColorPtr = void (__stdcall*)(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha);
+    using GLViewportPtr = void (__stdcall*)(GLint x, GLint y, GLsizei width, GLsizei height);
+    using GLGetStringPtr = const GLubyte* (__stdcall*)(GLenum name);
+    using GLGenBuffersPtr = void (__stdcall*)(GLsizei n, GLuint* buffers);
+    using GLDeleteBuffersPtr = void (__stdcall*)(GLsizei n, const GLuint* buffers);
+    using GLBindBufferPtr = void (__stdcall*)(GLenum target, GLuint buffer);
+    using GLBufferDataPtr = void (__stdcall*)(GLenum target, GLsizeiptr size, const void* data, GLenum usage);
+    using GLBufferSubDataPtr = void (__stdcall*)(GLenum target, GLintptr offset, GLsizeiptr size, const void* data);
+    using GLGenVertexArraysPtr = void(__stdcall*)(GLsizei n, GLuint* arrays);
+    using GLDeleteVertexArraysPtr = void(__stdcall*)(GLsizei n, const GLuint* arrays);
+    using GLBindVertexArrayPtr = void(__stdcall*)(GLuint array);
+    using GLDrawElementsPtr = void (__stdcall*)(GLenum mode, GLsizei count, GLenum type, const void* indices);
+    using GLDrawArraysPtr = void (__stdcall*)(GLenum mode, GLint first, GLsizei count);
+    using GLVertexAttribPointerPtr = void (__stdcall*)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void* pointer);
+    using GLEnableVertexAttribArrayPtr = void (__stdcall*)(GLuint index);
+    using GLCreateProgramPtr = GLuint (__stdcall*)(void);
+    using GLDeleteProgramPtr = void (__stdcall*)(GLuint program);
+    using GLLinkProgramPtr = void (__stdcall*)(GLuint program);
+    using GLUseProgramPtr = void (__stdcall*)(GLuint program);
+    using GLCreateShaderPtr = GLuint (__stdcall*)(GLenum shaderType);
+    using GLShaderSourcePtr = void (__stdcall*)(GLuint shader, GLsizei count, const GLchar** string, const GLint* length);
+    using GLCompileShaderPtr = void (__stdcall*)(GLuint shader);
+    using GLGetShaderivPtr = void (__stdcall*)(GLuint shader, GLenum pname, GLint* params);
+    using GLGetShaderInfoLogPtr = void (__stdcall*)(GLuint shader, GLsizei maxLength, GLsizei* length, GLchar* infoLog);
+    using GLAttachShaderPtr = void (__stdcall*)(GLuint program, GLuint shader);
+    using GLDeleteShaderPtr = void (__stdcall*)(GLuint shader);
+    using GLGetUniformLocationPtr = GLint (__stdcall*)(GLuint program, const GLchar* name);
+    using GLUniform1fPtr = void (__stdcall*)(GLint location, GLfloat v0);
+    using GLUniform1iPtr = void (__stdcall*)(GLint location, GLint v0);
+    using GLUniform2fvPtr = void (__stdcall*)(GLint location, GLsizei count, const GLfloat* value);
+    using GLUniform3fvPtr = void (__stdcall*)(GLint location, GLsizei count, const GLfloat* value);
+    using GLUniform4fvPtr = void (__stdcall*)(GLint location, GLsizei count, const GLfloat* value);
+    using GLUniformMatrix2fvPtr = void (__stdcall*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value);
+    using GLUniformMatrix3fvPtr = void (__stdcall*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value);
+    using GLUniformMatrix4fvPtr = void (__stdcall*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value);
 
-	extern GLClearPtr glClear;
-	extern GLClearColorPtr glClearColor;
-	extern GLViewportPtr glViewport;
-	extern GLGetStringPtr glGetString;
-	extern GLGenBuffersPtr glGenBuffers;
-	extern GLDeleteBuffersPtr glDeleteBuffers;
-	extern GLBindBufferPtr glBindBuffer;
-	extern GLBufferDataPtr glBufferData;
-	extern GLBufferSubDataPtr glBufferSubData;
-	extern GLGenVertexArraysPtr glGenVertexArrays;
-	extern GLDeleteVertexArraysPtr glDeleteVertexArrays;
-	extern GLBindVertexArrayPtr glBindVertexArray;
-	extern GLDrawElementsPtr glDrawElements;
-	extern GLDrawArraysPtr glDrawArrays;
-	extern GLVertexAttribPointerPtr glVertexAttribPointer;
-	extern GLEnableVertexAttribArrayPtr glEnableVertexAttribArray;
-	extern GLCreateProgramPtr glCreateProgram;
-	extern GLDeleteProgramPtr glDeleteProgram;
-	extern GLLinkProgramPtr glLinkProgram;
-	extern GLUseProgramPtr glUseProgram;
-	extern GLCreateShaderPtr glCreateShader;
-	extern GLShaderSourcePtr glShaderSource;
-	extern GLCompileShaderPtr glCompileShader;
-	extern GLGetShaderivPtr glGetShaderiv;
-	extern GLGetShaderInfoLogPtr glGetShaderInfoLog;
-	extern GLAttachShaderPtr glAttachShader;
-	extern GLDeleteShaderPtr glDeleteShader;
-	extern GLGetUniformLocationPtr glGetUniformLocation;
-	extern GLUniform1fPtr glUniform1f;
-	extern GLUniform1iPtr glUniform1i;
-	extern GLUniform2fvPtr glUniform2fv;
-	extern GLUniform3fvPtr glUniform3fv;
-	extern GLUniform4fvPtr glUniform4fv;
-	extern GLUniformMatrix2fvPtr glUniformMatrix2fv;
-	extern GLUniformMatrix3fvPtr glUniformMatrix3fv;
-	extern GLUniformMatrix4fvPtr glUniformMatrix4fv;
+    extern GLClearPtr glClear;
+    extern GLClearColorPtr glClearColor;
+    extern GLViewportPtr glViewport;
+    extern GLGetStringPtr glGetString;
+    extern GLGenBuffersPtr glGenBuffers;
+    extern GLDeleteBuffersPtr glDeleteBuffers;
+    extern GLBindBufferPtr glBindBuffer;
+    extern GLBufferDataPtr glBufferData;
+    extern GLBufferSubDataPtr glBufferSubData;
+    extern GLGenVertexArraysPtr glGenVertexArrays;
+    extern GLDeleteVertexArraysPtr glDeleteVertexArrays;
+    extern GLBindVertexArrayPtr glBindVertexArray;
+    extern GLDrawElementsPtr glDrawElements;
+    extern GLDrawArraysPtr glDrawArrays;
+    extern GLVertexAttribPointerPtr glVertexAttribPointer;
+    extern GLEnableVertexAttribArrayPtr glEnableVertexAttribArray;
+    extern GLCreateProgramPtr glCreateProgram;
+    extern GLDeleteProgramPtr glDeleteProgram;
+    extern GLLinkProgramPtr glLinkProgram;
+    extern GLUseProgramPtr glUseProgram;
+    extern GLCreateShaderPtr glCreateShader;
+    extern GLShaderSourcePtr glShaderSource;
+    extern GLCompileShaderPtr glCompileShader;
+    extern GLGetShaderivPtr glGetShaderiv;
+    extern GLGetShaderInfoLogPtr glGetShaderInfoLog;
+    extern GLAttachShaderPtr glAttachShader;
+    extern GLDeleteShaderPtr glDeleteShader;
+    extern GLGetUniformLocationPtr glGetUniformLocation;
+    extern GLUniform1fPtr glUniform1f;
+    extern GLUniform1iPtr glUniform1i;
+    extern GLUniform2fvPtr glUniform2fv;
+    extern GLUniform3fvPtr glUniform3fv;
+    extern GLUniform4fvPtr glUniform4fv;
+    extern GLUniformMatrix2fvPtr glUniformMatrix2fv;
+    extern GLUniformMatrix3fvPtr glUniformMatrix3fv;
+    extern GLUniformMatrix4fvPtr glUniformMatrix4fv;
 
-	bool initGL();
+    bool loadGLFunctions();
 
 }
