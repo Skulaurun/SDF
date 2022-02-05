ec54f7db1ddba23fcf7c2b80de55d4c6a439c44d
Skulaurun Mrusal <Skulaurun@seznam.cz>
2022-02-05 16:02:16 +0100

    Add: Image Decoder wrapper around WIC

---
+++ SDF/include/SDF/Graphics/Image.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <memory>
+#include <filesystem>
+
+// SDF Image Decoder
+// Currently works with BMP & PNG (JPG support planned)
+// There are some issues with specific PNGs tho
+// TODO: Fix this later
+
+// This is probably issue with the sdf::Texture class,
+// since we tell OpenGL that our pixel data is 32-bit in color depth (BGRA)
+
+typedef struct IWICStream IWICStream;
+typedef struct IWICImagingFactory IWICImagingFactory;
+
+namespace sdf {
+
+    class Image {
+
+    public:
+        Image(const std::filesystem::path& path);
+        Image(uint8_t* buffer, const uint32_t size);
+
+        ~Image();
+
+        uint8_t* getData() const { return data; }
+
+        uint32_t getWidth() const { return width; }
+        uint32_t getHeight() const { return height; }
+        uint32_t getBytesPerPixel() const { return bytesPerPixel; }
+
+    private:
+        Image();
+
+        void decodeImage(IWICStream* stream);
+
+    private:
+        uint8_t* data;
+
+        uint32_t width;
+        uint32_t height;
+        uint32_t bytesPerPixel;
+
+        static IWICImagingFactory* factory;
+
+    };
+
+}
---
+++ SDF/src/Graphics/Image.cpp
@@ -0,0 +1,121 @@
+#include "SDF/Graphics/Image.hpp"
+
+#include <windows.h>
+#include <wincodec.h>
+#include <comdef.h> // This is for _com_error
+
+namespace sdf {
+
+    IWICImagingFactory* Image::factory = nullptr;
+
+    Image::Image() : data(nullptr), width(0), height(0), bytesPerPixel(0) {
+
+        class ImageFactory {
+
+        public:
+            ImageFactory(IWICImagingFactory** factory) {
+
+                HRESULT hResult = S_OK;
+                hResult = CoInitialize(0);
+
+                hResult = CoCreateInstance(
+                    CLSID_WICImagingFactory,
+                    NULL,
+                    CLSCTX_INPROC_SERVER,
+                    IID_PPV_ARGS(factory)
+                );
+
+            }
+
+            ~ImageFactory() {
+                factory->Release();
+                CoUninitialize();
+            }
+
+        };
+
+        static ImageFactory staticFactory(&factory);
+
+    }
+    Image::~Image() {
+        free(data);
+    }
+
+    Image::Image(uint8_t* buffer, const uint32_t size) : Image() {
+
+        IWICStream* stream = NULL;
+        HRESULT hResult = factory->CreateStream(&stream);
+
+        stream->InitializeFromMemory(buffer, size);
+        decodeImage(stream);
+        stream->Release();
+
+    }
+
+    Image::Image(const std::filesystem::path& path) : Image() {
+
+        IWICStream* stream = NULL;
+        HRESULT hResult = factory->CreateStream(&stream);
+
+        stream->InitializeFromFilename(path.c_str(), GENERIC_READ);
+        decodeImage(stream);
+        stream->Release();
+
+    }
+
+    // TODO: Check HRESULTs and throw exceptions
+    void Image::decodeImage(IWICStream* stream) {
+
+        HRESULT hResult = S_OK;
+
+        IWICBitmapDecoder* decoder = NULL;
+        hResult = factory->CreateDecoderFromStream(
+            stream,
+            NULL,
+            WICDecodeMetadataCacheOnDemand,
+            &decoder
+        );
+
+        UINT frameCount = 0;
+        hResult = decoder->GetFrameCount(&frameCount);
+
+        IWICBitmapFrameDecode* decoderFrame = NULL;
+        hResult = decoder->GetFrame(0, &decoderFrame);
+
+        hResult = decoderFrame->GetSize((UINT*)&width, (UINT*)&height);
+
+        WICRect rect = { 0, 0, width, height };
+
+        WICPixelFormatGUID pixelFormatGUID;
+        hResult = decoderFrame->GetPixelFormat(&pixelFormatGUID);
+
+        IWICComponentInfo* componentInfo = NULL;
+        IWICPixelFormatInfo* pixelFormatInfo = NULL;
+        hResult = factory->CreateComponentInfo(pixelFormatGUID, &componentInfo);
+        hResult = componentInfo->QueryInterface(__uuidof(IWICPixelFormatInfo), (void**)&pixelFormatInfo);
+
+        hResult = pixelFormatInfo->GetBitsPerPixel(&bytesPerPixel);
+        bytesPerPixel /= 8;
+
+        UINT stride = bytesPerPixel * width;
+        UINT bufferSize = stride * height;
+
+        BYTE* buffer = (BYTE*)malloc(bufferSize);
+        if (buffer == nullptr) {
+            throw std::bad_alloc();
+        }
+
+        hResult = decoderFrame->CopyPixels(&rect, stride, bufferSize, buffer);
+        if (SUCCEEDED(hResult)) {
+            data = (uint8_t*)buffer;
+        } else {
+            free(buffer);
+        }
+
+        decoderFrame->Release();
+        pixelFormatInfo->Release();
+        componentInfo->Release();
+
+    }
+
+}
